a78de97cc1f5e11a41626982def9a9ed07d07ca5
jw <jwlee1728@gmail.com>
2018-03-02 08:38:50 +0900

    ch22

---
+++ tarc/ch22/server.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.urlencoded({extended:false}));
+
+var session = require('express-session');
+app.use(session({
+    resave:false,
+    saveUninitialized:false,
+    secret:'Secret Key'
+}));
+
+var passport = require('passport');
+app.use(passport.initialize());
+app.use(passport.session());
+
+var LocalStrategy = require('passport-local').Strategy;
+var strategy = new LocalStrategy(function(username, password, done){
+    if(username === 'user' && password === '1234'){
+        var user = {name:'사용자', email:'user@mail.com'};
+        done(null, user);
+    }
+    else
+        done(null, false, '로그인 실패');
+});
+
+passport.use(strategy);
+
+passport.serializeUser(function(user, done){
+    console.log('세션에 기록하기');
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done){
+    console.log('세션에서 사용자 정보 조회하기 하기');
+    done(null, user);
+});
+
+app.post('/login', passport.authenticate('local'), function(req, res){ res.send('success')});
+app.get('/personal', showPersonal);
+app.listen(3000);
+
+function showPersonal(req, res){
+    console.log('req.user : ', req.user);
+    var user = req.user;
+    if( user )
+        res.send('Personal page ' + user.name);
+    else
+        res.sendStatus(401);
+}
