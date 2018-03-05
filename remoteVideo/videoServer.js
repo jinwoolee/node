d339d1967aa39eddeb013f70a2ed92908662351b
jw <jwlee1728@gmail.com>
2018-03-05 21:44:26 +0900

    cam

---
+++ remoteVideo/videoServer.js
@@ -0,0 +1,60 @@
+var http = require("http");
+var fs = require("fs");
+var websocket = require("websocket").server;
+
+var connection = [];
+var webrtc_discussions = {};
+
+var httpServer = http.createServer(function(req, res){
+    
+    console.log('req.url : ', req.url);
+
+    if(req.url.indexOf('view') > 0){
+        fs.readFile('./videoClient.html', function(err, data){
+            res.end(data);
+        });
+    }
+    else if(req.url.indexOf('list') > 0){
+        res.end(connection.toString());
+    }
+});
+
+var websocket_server = new websocket({
+    httpServer: httpServer/*,
+    autoAcceptConnections : false*/
+});
+
+//websocket_server.on('connect', function(conn){
+websocket_server.on('request', function(request){
+    console.log('connected');
+
+    var conn = request.accept(null, request.origin);
+    connection.push(conn);
+
+    conn.id = connection.length-1;    
+
+    conn.on('message', function(message){
+        console.log('message : ', message);
+
+        if (message.type === "utf8") {
+            var signal = JSON.parse(message.utf8Data);
+
+            if (signal.type === "join" && signal.token !== undefined) {
+                if (webrtc_discussions[signal.token] === undefined) {
+                    webrtc_discussions[signal.token] = {};
+                }
+
+                webrtc_discussions[signal.token][conn.id] = true;
+            }
+            else if (signal.token !== undefined) {
+                Object.keys(webrtc_discussions[signal.token]).forEach(function(id) {
+                    if (id != conn.id) {
+                        webrtc_clients[id].send(message.utf8Data);
+                    }
+                });
+            } 
+        }
+    });
+})
+
+httpServer.listen(3000);
