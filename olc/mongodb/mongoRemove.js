27e7bebe7f66f65d2ff4cd2146867daaeef4c3ca
jw <jwlee1728@gmail.com>
2018-03-19 16:29:33 +0900

    olc mongodb

---
+++ olc/mongodb/mongoRemove.js
@@ -0,0 +1,65 @@
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+var insertDocuments = function(database, callback) {
+  // Get the documents collection
+  var db = database.db('myproject')
+  var collection = db.collection('documents')
+
+  //var collection = db.collection('documents');
+
+
+  // Insert some documents
+  collection.insert([
+    {a : 1}, {a : 2}, {a : 3}
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the document collection");
+    callback(result);
+  });
+}
+
+var updateDocument = function(database, callback) {
+  var db = database.db('myproject')
+  var collection = db.collection('documents')
+
+  // Update document where a is 2, set b equal to 1
+  collection.update({ a : 2 }
+    , { $set: { b : 2 } }, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    console.log("Updated the document with the field a equal to 2");
+    callback(result);
+  });
+}
+
+var removeDocument = function(database, callback) {
+  // Get the documents collection
+  var collection = database.db('myproject').collection('documents');
+
+  // Insert some documents
+  collection.remove({ a : 3 }, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    console.log("Removed the document with the field a equal to 3");
+    callback(result);
+  });
+}
+
+// Connection URL
+var url = 'mongodb://localhost:27017/myproject';
+// Use connect method to connect to the Server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+ 
+  insertDocuments(db, function() {
+    updateDocument(db, function() {
+      removeDocument(db, function() {
+        db.close();
+      });
+    });
+  });
+});
